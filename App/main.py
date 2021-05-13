0b2d5a35f5a57602a8c2fb6e443d90bc5030702e
sepy485 <56456163+sepy485@users.noreply.github.com>
2021-05-12 20:18:57 -0400

    Create main.py
    Creación de las variables

---
+++ App/main.py
@@ -0,0 +1,20 @@
+import numpy as np
+N = 4
+N = int(input("Ingrese el número de ciudades a recorrer: "))
+print('Algoritmo del viajero! \n')
+print('Ciudades: \n')
+for i in range(N):
+  print('C' + str(i+1))
+
+print(' \n')
+print('Distancia de ciudades')
+matriz = np.random.randint(99, size=(N, N)) + 1
+print(matriz)
+print(' \n')
+#Función que le asigna 0 a la distancia de una ciudad a sí misma
+for x in range(N):
+  for y in range(N):
+    if(x == y):
+      matriz[x][y] = 0
+
+print(matriz)
