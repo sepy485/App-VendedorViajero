e5c3c6cc70f87d54b5705f323c696f9a7bf33732
sepy485 <56456163+sepy485@users.noreply.github.com>
2021-05-12 20:24:54 -0400

    Update main.py

--- App/main.py
+++ App/main.py
@@ -1,20 +1,20 @@
 import numpy as np
 N = 4
+ciudades = 'C1'
+print('Bienvenido al algoritmo del viajero! \n')
 N = int(input("Ingrese el número de ciudades a recorrer: "))
-print('Algoritmo del viajero! \n')
-print('Ciudades: \n')
-for i in range(N):
-  print('C' + str(i+1))
-
+print('Ciudades: ')
+for i in range(N-1):
+  ciudades = (ciudades + ', C' + str(i+1))
+print(ciudades)
 print(' \n')
 print('Distancia de ciudades')
 matriz = np.random.randint(99, size=(N, N)) + 1
-print(matriz)
-print(' \n')
+
 #Función que le asigna 0 a la distancia de una ciudad a sí misma (diagonal de ceros)
 for x in range(N):
   for y in range(N):
     if(x == y):
       matriz[x][y] = 0
-
 print(matriz)
+print(' \n')
