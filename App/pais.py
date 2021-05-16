73894965648e048b217d11ee7ff5518285d6920c
jaaz-97 <javier.araya@alumnos.ucn.cl>
2021-05-15 22:08:22 -0400

    Update pais.py

--- App/pais.py
+++ App/pais.py
@@ -1,2 +1,10 @@
 class Pais:
     nombre = ''
+    visitado = False
+
+    def getVisitado(self):
+        return self.visitado
+
+    def setVisitado(self, newVisitado):
+        self.visitado = newVisitado
+ 
