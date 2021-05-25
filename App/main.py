750cfa52086f744ac2609c0c2815b652b7f7adf2
sepy485 <56456163+sepy485@users.noreply.github.com>
2021-05-24 22:04:11 -0400

    Código final
    Código final comentado

--- App/main.py
+++ App/main.py
@@ -1,36 +1,37 @@
 import numpy as np
 from Ciudad import Ciudad
 import time
-N = 0
 
-print('Bienvenido al algoritmo del viajero! \n')
+#Variables
+N = 0 #Cantidad de ciudades INPUT
 listaCiudades = []
 
+
+#Inicio del programa
+print('Bienvenido al algoritmo del viajero! \n')
 while(N < 4 or N%2 != 0):
     N = int(input("Ingrese el número de ciudades a recorrer: "))
     if(N<4 or N%2 != 0):
         print("N debe ser mayor o igual a 4 y un número par!")
         
-        
-print('Ciudades: ')
+#Rellena una lista con las ciudades
 for i in range(N):
   nombre = 'C' + str(i)
   listaCiudades.append(Ciudad(i,nombre))
 
 print(' \n')
 
-print('Distancia de ciudades')
+#Generamos una matriz de números aleatorios entre 1 y 100 para simular las distancias
 matriz = np.random.randint(99, size=(N, N)) + 1
-
-#Crear NODOS
-
-#Crear árbol
 
 #Función que le asigna 0 a la distancia de una ciudad a sí misma (diagonal de ceros)
 for x in range(N):
   for y in range(N):
     if(x == y):
       matriz[x][y] = 0
+      
+#Muestra la matriz de distancias entre ciudades
+print('Distancia de ciudades')
 print(matriz)
 print(' \n')
 
@@ -39,20 +40,12 @@ def calcularDistancia(ciudad1, ciudad2, matriz):
   distancia = (matriz[ciudad1][ciudad2])
   return distancia
       
-print(str(calcularDistancia(0,3,matriz)))
-
-print("Tiempo transcurrido: " + str(time.process_time()) + " [sg]")
-
-
-
-
-
-
+#AlgoritmoDFS (recursivo) que calcula el camino que toma el viajero
 def algoritmoDFS(listaCiudades, matrizDistancias, N, resultado):
-  nodo1 = listaCiudades[0] #toma cualquier ciudad para iniciar, en este caso la primera
-  nodo1.setVisitado(True)
+  nodo1 = listaCiudades[0] #toma cualquier ciudad para iniciar, en este caso la primera de la lista
+  nodo1.setVisitado(True) #Marca la ciudad como visitada
   listaConectados = listaCiudades #toma las ciudades conectadas
-  listaConectados.pop(0)
+  listaConectados.pop(0) #Quitamos de la lista la ciudad visitada
   i = len(listaConectados)
  
   #Si se llegó al final de la lista finaliza  
@@ -61,11 +54,12 @@ def algoritmoDFS(listaCiudades, matrizDistancias, N, resultado):
     print("El resultado es: " + str(resultado))
     print("Tiempo transcurrido: " + str(time.process_time()) + " [sg]")
     return resultado
-  #Si no ha terminado con la lista
+  #Si no ha terminado con la lista entonces continúa
   else:
     nodo2 = listaConectados[0]
     resultado = resultado + nodo1.getNombre() + " => " + str(calcularDistancia(nodo1.getNumero(),nodo2.getNumero(),matriz)) + " => "
     algoritmoDFS(listaConectados, matrizDistancias, N, resultado)
     return resultado
 
+#Se ejecuta el algoritmo
 algoritmoDFS(listaCiudades, matriz, N, '')
